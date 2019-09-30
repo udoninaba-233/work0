b68a963ffcbcb373bedd4466c42afbb81b4f2002
udoninaba-233 <55790980+udoninaba-233@users.noreply.github.com>
2019-09-30 15:22:02 +0800

    Add files via upload

---
+++ work0 second try.cpp
@@ -0,0 +1,29 @@
+//work0 second try
+
+//00x01
+#include<iostream>
+
+int main()
+{
+	using namespace std;
+	cout << "HelloWorld!";
+	cout << endl;
+	cout << "\n";
+	cout << "******" << endl;
+	cout << "I LOVE C!" << endl;
+	cout << "******" << endl;
+	return 0;
+}  
+
+//00x02
+
+/*#include<stdio.h>
+int main()
+{
+	printf("HelloWorld!\n\n");
+	printf("******\n");
+	printf("I LOVE C!\n");
+	printf("******\n")	;
+	
+	return 0;
+}*/
